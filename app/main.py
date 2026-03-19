36d5dbe7e8a2ef810e7f333d5379044c8c5ecd26
Aiman-xo <aimanrasack@gmail.com>
2026-03-19 11:28:24 +0530

    fixed the CORS issue

--- app/main.py
+++ app/main.py
@@ -21,7 +21,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
-    # "https://your-production-app.render.com",
+    "https://docbot-ai-h0hr.onrender.com",
+    "https://doc-bot-ai.vercel.app"
 ]
 
 app.add_middleware(
